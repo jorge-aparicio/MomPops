374846104f71726e977280edb29e33589652ab78
alexmfranklin <amfa2017@mymail.pomona.edu>
2020-07-24 10:08:26 +0800

    basic layout and wheel button

--- app/src/main/java/com/example/mom_pops/MainActivity.java
+++ app/src/main/java/com/example/mom_pops/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mom_pops;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,6 +14,8 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button spin_the_wheel_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,18 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        spin_the_wheel_button = (Button) findViewById(R.id.spin_the_wheel);
+        spin_the_wheel_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                open_spin_the_wheel();
+            }
+        });
     }
 
+    public void open_spin_the_wheel() {
+        Intent intent = new Intent(this, SpinTheWheel.class);
+        startActivity(intent);
+    }
 }
---
+++ app/src/main/java/com/example/mom_pops/MyFavorites.java
@@ -0,0 +1,14 @@
+package com.example.mom_pops;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyFavorites extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_favorites);
+    }
+}
