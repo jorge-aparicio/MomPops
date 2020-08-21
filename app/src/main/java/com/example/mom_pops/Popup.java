d066f8b4e628f6636a0af695ee77e8028eba5cde
Jason Cabrera <cabrerajason7@gmail.com>
2020-08-21 16:22:25 -0700

    finished confirmation popup for clear cart

--- app/src/main/java/com/example/mom_pops/Popup.java
+++ app/src/main/java/com/example/mom_pops/Popup.java
@@ -1,7 +1,6 @@
 package com.example.mom_pops;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -9,10 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.cardview.widget.CardView;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 public class Popup extends Activity {
     private String itemName;
@@ -75,12 +70,33 @@ public class Popup extends Activity {
                 break;
 
             case "confirmation":
-                getWindow().setLayout((int) (width * .65), (int) (height * .65));
-                setContentView(R.layout.activity_cart_item_pop);
+                getWindow().setLayout((int) (width * .65), (int) (height * .40));
+                setContentView(R.layout.activity_confirmation_pop);
                 String message = intent.getStringExtra("message");
                 TextView message_textView = findViewById(R.id.confirmation_text);
                 message_textView.setText(message);
 
+                Button cancelButton = findViewById(R.id.cancel_button);
+                Button confirmButton = findViewById(R.id.confirm_button);
+                cancelButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent res = new Intent();
+                        res.putExtra("result", false);
+                        setResult(1, res);
+                        finish();
+                    }
+                });
+
+                confirmButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent res = new Intent();
+                        res.putExtra("result", true);
+                        setResult(1, res);
+                        finish();
+                    }
+                });
         }
     }
 }
--- app/src/main/java/com/example/mom_pops/ViewCartActivity.java
+++ app/src/main/java/com/example/mom_pops/ViewCartActivity.java
@@ -14,6 +14,7 @@ import static java.security.AccessController.getContext;
 
 public class ViewCartActivity extends AppCompatActivity {
     private Activity activity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         activity = this;
@@ -25,21 +26,36 @@ public class ViewCartActivity extends AppCompatActivity {
         clearCart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                FrameLayout cartContainer = findViewById(R.id.cartFrame);
+                if (cartContainer.getChildCount() == 0) {
+                    Toast.makeText(getApplicationContext(), "Cart is already empty.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 // creates popup asking for confirmation
                 Intent intent = new Intent(activity, Popup.class);
                 intent.putExtra("popupType", "confirmation");
                 intent.putExtra("message", "Clear Cart?");
-                startActivity(intent);
-
-                FrameLayout cartContainer = findViewById(R.id.cartFrame);
-                if (cartContainer.getChildCount() == 0) {
-                    Toast.makeText(getApplicationContext(), "Cart is already empty.", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "Clearing Cart...", Toast.LENGTH_SHORT).show();
-                    Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_SHORT).show();
-                    cartContainer.removeAllViews();
-                }
+                startActivityForResult(intent, 1);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // clear cart request code
+        if (requestCode == 1) {
+            boolean result = data.getBooleanExtra("result", false);
+
+            // if user wants to delete cart
+            if (result) {
+                FrameLayout cartContainer = findViewById(R.id.cartFrame);
+                Toast.makeText(getApplicationContext(), "Clearing Cart...", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_SHORT).show();
+                cartContainer.removeAllViews();
+            }
+        }
     }
 }
