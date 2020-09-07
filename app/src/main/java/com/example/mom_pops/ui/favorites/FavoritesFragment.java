149ccb3b7007c471ce37875e4ed674a6c32a0c30
Jason Cabrera <cabrerajason7@gmail.com>
2020-09-07 16:09:57 -0700

    refactored dashboard to favorites

--- app/src/main/java/com/example/mom_pops/ui/favorites/FavoritesFragment.java
+++ app/src/main/java/com/example/mom_pops/ui/favorites/FavoritesFragment.java
@@ -1,4 +1,4 @@
-package com.example.mom_pops.ui.dashboard;
+package com.example.mom_pops.ui.favorites;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.mom_pops.R;
 
-public class DashboardFragment extends Fragment {
+public class FavoritesFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
+    private FavoritesViewModel favoritesViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
+        favoritesViewModel =
+                ViewModelProviders.of(this).get(FavoritesViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        favoritesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
