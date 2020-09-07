a6d97ea188975062393f0b14245f2c75df484ab5
Jason Cabrera <cabrerajason7@gmail.com>
2020-09-07 16:02:03 -0700

    changed navbar icons/labels

--- app/src/main/java/com/example/mom_pops/MainActivity.java
+++ app/src/main/java/com/example/mom_pops/MainActivity.java
@@ -15,42 +15,37 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    getSupportActionBar().hide();
+    setContentView(R.layout.activity_main);
 
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
-    }
+    BottomNavigationView navView = findViewById(R.id.nav_view);
+    // Passing each menu ID as a set of Ids because each
+    // menu should be considered as top level destinations.
+    AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+            R.id.navigation_home, R.id.navigation_favorites, R.id.navigation_viewcart)
+            .build();
+    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    NavigationUI.setupWithNavController(navView, navController);
+  }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.activity_main, menu);
+    return true;
+  }
 
-    public void goToMenuPage(View v) {
-        Intent intent = new Intent(this, ClickedRestaurantActivity.class);
-        startActivity(intent);
-    }
+  public void goToMenuPage(View v) {
+    Intent intent = new Intent(this, ClickedRestaurantActivity.class);
+    startActivity(intent);
+  }
 
-    public void goToEditRestaurantPage(View v) {
-        Intent intent = new Intent(this, EditRestaurantMenuActivity.class);
-        startActivity(intent);
-    }
-
-    public void goToViewCart(View v) {
-        Intent intent = new Intent(this, ViewCartActivity.class);
-        startActivity(intent);
-    }
+  public void goToEditRestaurantPage(View v) {
+    Intent intent = new Intent(this, EditRestaurantMenuActivity.class);
+    startActivity(intent);
+  }
 }
